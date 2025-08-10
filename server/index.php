76a929fa1728077604af69cfc37f2766383ab608
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-08-10 16:43:47 +0900

    response header

--- server/index.php
+++ server/index.php
@@ -1,2 +1,10 @@
 <?php session_start();
 date_default_timezone_set('Asia/Tokyo');
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Content-Length, Origin, Accept, Access-Control-Allow-Headers, X-Token");
+
+header('Server: Hidden');
+header('X-Powered-By: Hidden');
+
