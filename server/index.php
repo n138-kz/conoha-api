1d3ebdc622c77c150c4bc347ff12b8573dc1b3e1
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-08-11 18:54:11 +0900

    autoload vendor/autoload.php

--- server/index.php
+++ server/index.php
@@ -1,4 +1,7 @@
 <?php session_start();
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    include_once(__DIR__ . '/../vendor/autoload.php');
+}
 date_default_timezone_set('Asia/Tokyo');
 header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
